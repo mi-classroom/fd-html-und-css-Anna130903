7a6479e3b71ec029adb6174e9b02a74d91c87b50
Anna Stein <anna.stein@smail.th-koeln.de>
2025-06-20 15:47:45 +0200

    Added javascript for onpageNavigation

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,5 @@
+/* Menu
+############################################################################ */
 const menuOpenButton = document.querySelector('[data-js-main-menu-open]');
 const menuOverlay = document.querySelector('[data-js-main-menu-target]');
 const menuCloseButton = document.querySelector('[data-js-main-menu-close]');
@@ -9,3 +11,34 @@ menuOpenButton.addEventListener('click', function() {
 menuCloseButton.addEventListener('click', function() {
   menuOverlay.classList.remove('is-active');
 });
+
+
+
+/* On Page Navigation
+############################################################################ */
+
+const sectionHeadlines = document.querySelectorAll("[data-js-page-navigation-content] > section > h2");
+const pageNavigation = document.querySelector('[data-js-page-navigation-menu]');
+
+sectionHeadlines.forEach(function (headline) {
+
+  const sectionID = headline.innerText.toLowerCase().replaceAll(' ', '-');
+  const pageNavigationItem = document.createElement('li');
+
+  const pageNavigationAnchor = document.createElement('a');
+  pageNavigationAnchor.innerText = headline.innerText;
+  pageNavigationAnchor.setAttribute('href', '#' + sectionID);
+
+  pageNavigationItem.appendChild(pageNavigationAnchor);
+
+  pageNavigation.appendChild(pageNavigationItem);
+
+  headline.parentElement.setAttribute('id', sectionID);
+
+});
+
+
+/*Search
+############################################################################### */
+
+
